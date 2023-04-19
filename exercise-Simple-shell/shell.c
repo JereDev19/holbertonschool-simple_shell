becdcae68877582cbf7d22dd9b5e6792087cc12a
jeremias <jeremiaserba7894@gmail.com>
2023-04-19 15:22:58 -0700

    Big big changes broo

--- exercise-Simple-shell/shell.c
+++ exercise-Simple-shell/shell.c
@@ -4,36 +4,60 @@ int main(void)
 {
 	ssize_t chars_read = 0;
 	size_t sizeBuffer = 0;
-	char *bufferEntry = NULL, *comandPath = NULL;
-	int status = 0;
+	char *bufferEntry = NULL, *comandPath = NULL, *command = NULL, *token = NULL;
+	int status = 0, i = 0;
 
-	bufferEntry = malloc(sizeof(char *) * sizeBuffer);
+	bufferEntry = malloc(sizeof(char) * sizeBuffer);
 	if (!bufferEntry)
-		return (1);
+		return (2);
 
 	while (1)
 	{
 		(isatty(STDOUT_FILENO) == 1 ? write(1, "$ ", 2) : 0);
 		chars_read = getline(&bufferEntry, &sizeBuffer, stdin);
+		token = strtok(bufferEntry, "\t \n");
+		
+		char **args = malloc(sizeof(char *) * sizeBuffer);
+		if (!args)
+			return (2);
 
+		while (token != NULL) 
+		{
+			args[i] = token;
+			token = strtok(NULL, " \n");
+ 			i++;
+		}
 		/* -- Verify if the command is exit or the user place exit --*/
 		if (chars_read == -1 || strcmp(bufferEntry, "exit\n") == 0)
 			((bufferEntry) ? free(bufferEntry), exit(status) : 0);
-
+		
 		else if (strcmp(bufferEntry, "env\n") == 0)
 			print_env();
 
 		/*path copy so not modify the original buffer*/
 		comandPath = strdup(bufferEntry);
-
-		/**
-		 * Pasos generales: (nos vamos a encontrar 30 mil error en el medio)
-		 *
-		 * Step 1:
-		 *  - Hacer funcion que devuelva un array con los parametros dados (en cada indice un determinado parametro o argumento).
-		 *
-		 * Step 2:
-		 *  - Buscar el path de ese argumento que obtuvimos y ejecutar el comando.
-		 */
+		command = get_path(comandPath);
+		if (command != NULL)
+		{
+			pid_t child = fork();
+		
+			if (child > 0)
+			{
+				wait(&status);
+			}
+			else if (child == 0)
+			{
+				execve(command, args, environ);
+				free(command);
+			}
+			else
+			{
+				perror("Error");
+				free(command);
+				return (1);
+			}
+		free(args[0]);
+		free(args);
+		}
 	}
 }
