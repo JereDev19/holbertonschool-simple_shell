ed99f3e1188468db23af17751999cf64e698764b
jeremias <jeremiaserba7894@gmail.com>
2023-04-21 20:08:31 -0700

    Simple shell - Jeremias Erba | Gabriel Acosta. - First version simple shell.

--- shell.c
+++ shell.c
@@ -1,17 +1,20 @@
 #include "shell.h"
+
 /**
-* main - replicate of a shell
-* Return: FALTA DETERMINAR LOS VALORES DE RETURN
+* main - replicate of a shell.
+* @args: cant of params.
+* @argv: values of params.
+* Return: Status.
 */
+
 int main(int argc, char *argv[])
 {
 	size_t sizeBuffer = 0;
-	char *command = NULL, *comandPathCopy = NULL; 
-	char *bufferEntry = NULL, **args = NULL;
+	char *command = NULL, *comandPathCopy = NULL, bufferEntry = NULL;
+	char **args = NULL;
 	int status = 0, satty = isatty(STDOUT_FILENO), count = 0;
 	(void)argc;
 	satty == 1 ? write(1, "$ ", 2) : 0;
-
 	while (getline(&bufferEntry, &sizeBuffer, stdin) >= 0)
 	{
 		count += 1;
@@ -20,30 +23,24 @@ int main(int argc, char *argv[])
 			satty == 1 ? write(1, "$ ", 2) : 0;
 			continue;
 		}
-			if (strcmp(bufferEntry, "exit\n") == 0)
+		if (strcmp(bufferEntry, "exit\n") == 0)
 			break;
-			args = generate_args(bufferEntry);
-			if (!args)
+		args = generate_args(bufferEntry);
+		if (!args)
 			return (2);
-			if (strcmp(bufferEntry, "env\n") == 0)
+		if (strcmp(bufferEntry, "env\n") == 0)
 			print_env();
+		else
+		{
+			command = get_path(args[0]);
+			if (command)
+				status = forkProcess(command, args), free(command);
 			else
-			{
-				command = get_path(args[0]);
-				if (command)
-				{
-					status = forkProcess(command, args);
-					free(command);
-				}
-				else
-				{
-					printErr(count, argv[0], bufferEntry);
-					status = 127;
-				}
-			}
-			free(args);
-			free(comandPathCopy);
-			satty == 1 ? write(1, "$ ", 2) : 0;
+				printErr(count, argv[0], bufferEntry), status = 127;
+		}
+		free(args);
+		free(comandPathCopy);
+		satty == 1 ? write(1, "$ ", 2) : 0;
 	}
 	free(bufferEntry);
 	return (status);
