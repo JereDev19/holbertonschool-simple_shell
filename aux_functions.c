3cedf10769fc7c0db49efdab27edfc75eca92dcd
jeremias <jeremiaserba7894@gmail.com>
2023-04-23 14:47:54 -0700

    Simple shell - Second version 1 - To create string functions for to test.

--- aux_functions.c
+++ aux_functions.c
@@ -6,14 +6,37 @@
  * Exercise 0.
  */
 
-void _print_env(void)
+void print_env(void)
 {
 	int i;
 
 	for (i = 0; environ && environ[i]; i++)
 	{
-		puts(environ[i]);
-		putchar('\n');
+		_puts(environ[i]);
+		_putchar('\n');
+	}
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char character)
+{
+	return (write(1, &character, 1));
+}
+
+void _puts(char *string)
+{
+	int i = 0;
+
+	while (*(string + i))
+	{
+		_putchar(*(string + i));
+		i++;
 	}
 }
 
