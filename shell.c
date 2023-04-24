14c5a814edf6f3efd7690600b6490e1aa54e98bf
jeremias <jeremiaserba7894@gmail.com>
2023-04-24 12:50:57 -0700

    Simple shell version 2

--- shell.c
+++ shell.c
@@ -1,17 +1,17 @@
 #include "shell.h"
 
 /**
-* main - replicate of a shell.
-* @argc: number of params.
-* @argv: array with values of paramams.
-* Return:  return exit status of last program.
+ * main - replicate of a shell.
+ * @argc: number of params.
+ * @argv: array with values of paramams.
+ * Return:  return exit status of last program.
 */
 
 int main(int argc, char *argv[])
 {
 	size_t sizeBuffer = 0;
 	char *command = NULL, *bufferEntry = NULL, **args = NULL;
-	int status = 0, satty = isatty(STDOUT_FILENO), count = 0;
+	int status = 0, satty = isatty(STDOUT_FILENO), count = 0, i = 0;
 	struct stat buffer;
 	(void)argc;
 	satty == 1 ? write(1, "$ ", 2) : 0;
@@ -19,24 +19,17 @@ int main(int argc, char *argv[])
 	while (getline(&bufferEntry, &sizeBuffer, stdin) >= 0)
 	{
 		count += 1;
-		args = generate_args(bufferEntry);
-		if (!args)
-			return (2);
 		if (strlen(bufferEntry) == 1)
 		{
 			satty == 1 ? write(1, "$ ", 2) : 0;
-			free_array(args);
 			continue;
 		}
-		if (strcmp(bufferEntry, "exit\n") == 0)
-		{
-			if (bufferEntry)
-				free(bufferEntry);
-			exit(status);
-		}
-		if (strcmp(args[0], "env\n") == 0)
+		else if (strcmp(bufferEntry, "exit\n") == 0)
+			(bufferEntry) ? free(bufferEntry), exit(status) : (0);
+		if (strcmp(bufferEntry, "env\n") == 0)
 			print_env();
-		else
+		args = generate_args(bufferEntry);
+		if (args && args[0])
 		{
 			if (stat(args[0], &buffer) == 0)
 			{
@@ -46,21 +39,12 @@ int main(int argc, char *argv[])
 			else
 			{
 				command = get_path(args[0]);
-				if (command)
-				{
-					status = forkProcess(command, args);
-					free(command);
-				}
-				else
-				{
-					printErr(count, argv[0], bufferEntry);
-					status = 127;
-				}
+				((command) ? (status = forkProcess(command, args), free(command))
+           			: printErr(count, argv[0], bufferEntry));
+				status = 127;
 			}
-			
 		}
-		free(args);
-		satty == 1 ? write(1, "$ ", 2) : 0;
+		free(args), satty == 1 ? write(1, "$ ", 2) : 0;
 	}
 	free(bufferEntry);
 	return (status);
